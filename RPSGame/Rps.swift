48f0aec0a862fc493091688f233749a314edc97c
DongMin Hwang <hdmin8712@gmail.com>
2022-11-14 19:04:39 +0900

    Initial Commit

---
+++ RPSGame/Rps.swift
@@ -0,0 +1,15 @@
+//
+//  RPS.swift
+//  RPSGame
+//
+//  Created by DongMin Hwang on 2022/11/14.
+//
+
+import Foundation
+
+// 원시값(raw value)
+enum Rps: Int {
+    case rock
+    case paper
+    case scissors
+}
